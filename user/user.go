d690cf977a21edb5c6aac54582758f2afe739511
Mustafa Berat <mbkaya879@gmail.com>
2024-05-14 01:32:47 +0300

    feat(user): Dto has been added .

--- user/user.go
+++ user/user.go
@@ -8,3 +8,9 @@ type User struct {
 	FullName string             `json:"fullName"`
 	Password string             `json:"password"`
 }
+
+type CreateUserRequest struct {
+	Nickname string `json:"nickname"`
+	FullName string `json:"fullName"`
+	Password string `json:"password"`
+}
