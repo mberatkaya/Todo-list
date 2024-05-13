829abaf1f1e1dd4e1949fd827922c434c194f85f
Mustafa Berat <mbkaya879@gmail.com>
2024-05-13 18:36:18 +0300

    fix(todo): Using reqdef.

--- todo/handler.go
+++ todo/handler.go
@@ -17,11 +17,8 @@ func (h *TodoHandler) RegisterRoutes(app *fiber.App) {
 	appGroup := app.Group("/api/todo") // API rotası için grup oluştur
 
 	appGroup.Get("/", h.GetAllTodos)
-
 	appGroup.Post("/", h.CreateTodo)
-
 	appGroup.Put("/:id", h.UpdateTodoCompletion)
-
 	appGroup.Delete("/:id", h.DeleteTodo)
 }
 
@@ -34,15 +31,12 @@ func (h *TodoHandler) GetAllTodos(c *fiber.Ctx) error {
 }
 
 func (h *TodoHandler) CreateTodo(c *fiber.Ctx) error {
-	var createTodoDto struct {
-		Task string `json:"task"`
-	}
-
-	if err := c.BodyParser(&createTodoDto); err != nil {
+	var req CreateTodoRequest
+	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request payload"})
 	}
 
-	todo, err := h.Service.CreateTodo(c.Context(), createTodoDto.Task)
+	todo, err := h.Service.CreateTodo(c.Context(), req.Task)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
@@ -57,15 +51,12 @@ func (h *TodoHandler) UpdateTodoCompletion(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID"})
 	}
 
-	var updatedTodo struct {
-		Completed bool `json:"completed"`
-	}
-
-	if err := c.BodyParser(&updatedTodo); err != nil {
+	var req UpdateTodoRequest
+	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request payload"})
 	}
 
-	if err := h.Service.UpdateTodoCompletion(c.Context(), objectID, updatedTodo.Completed); err != nil {
+	if err := h.Service.UpdateTodoCompletion(c.Context(), objectID, req.Completed); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
 
