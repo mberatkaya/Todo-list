0a0e9218bcca9f0ee33b15768260d61b0f30dd60
Mustafa Berat <mbkaya879@gmail.com>
2024-05-13 18:40:45 +0300

    feat(user): Add update endpoint and password for user profile.

--- user/handler.go
+++ user/handler.go
@@ -1,8 +1,10 @@
 package user
 
 import (
+	"errors"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserHandler struct {
@@ -14,27 +16,29 @@ func NewUserHandler(service *UserService) *UserHandler {
 }
 
 func (h *UserHandler) RegisterRoutes(app *fiber.App) {
-
 	appGroup := app.Group("/api/user")
 
 	appGroup.Post("/", h.CreateUser)
 	appGroup.Get("/:id", h.GetUser)
+	appGroup.Put("/:id", h.UpdateUser)
 	appGroup.Delete("/:id", h.DeleteUser)
 }
 
 func (h *UserHandler) CreateUser(c *fiber.Ctx) error {
-	var createUserDto struct {
-		Nickname string `json:"nickname"`
-		FullName string `json:"fullName"`
-	}
-
-	if err := c.BodyParser(&createUserDto); err != nil {
+	var req CreateUserRequest
+	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request payload"})
 	}
 
-	user, err := h.Service.CreateUser(c.Context(), createUserDto.Nickname, createUserDto.FullName)
+	_, err := h.Service.GetUserByNickname(c.Context(), req.Nickname)
+	if err == nil {
+		return c.Status(fiber.StatusConflict).JSON(fiber.Map{"error": "Nickname already exists"})
+	}
+
+	// Create user with password
+	user, err := h.Service.CreateUser(c.Context(), req.Nickname, req.FullName, req.Password)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error})
 	}
 
 	return c.Status(fiber.StatusCreated).JSON(user)
@@ -49,10 +53,32 @@ func (h *UserHandler) GetUser(c *fiber.Ctx) error {
 
 	user, err := h.Service.GetUserByID(c.Context(), objectID)
 	if err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	return c.JSON(user)
+}
+
+func (h *UserHandler) UpdateUser(c *fiber.Ctx) error {
+	idParam := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(idParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID"})
+	}
+
+	var req CreateUserRequest
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request payload"})
+	}
+
+	if err := h.Service.UpdateUser(c.Context(), objectID, req.Nickname, req.FullName); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func (h *UserHandler) DeleteUser(c *fiber.Ctx) error {
