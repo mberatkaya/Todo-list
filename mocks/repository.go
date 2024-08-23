de1aecabec18da9a3587401436f0b832609ac2d1
Mustafa Berat <mbkaya879@gmail.com>
2024-08-23 13:19:04 +0300

    feat(test)  Adding mock repository for the todo/service unit test .
    .

---
+++ mocks/repository.go
@@ -0,0 +1,32 @@
+package mocks
+
+import (
+	"TODOproject/todo"
+	"context"
+	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type MockTodoRepository struct {
+	mock.Mock
+}
+
+func (m *MockTodoRepository) GetAllTodos(ctx context.Context) ([]todo.Todo, error) {
+	args := m.Called(ctx)
+	return args.Get(0).([]todo.Todo), args.Error(1)
+}
+
+func (m *MockTodoRepository) CreateTodo(ctx context.Context, todo *todo.Todo) (*todo.Todo, error) {
+	args := m.Called(ctx, todo)
+	return args.Get(0).(*todo.Todo), args.Error(1)
+}
+
+func (m *MockTodoRepository) UpdateTodoCompletion(ctx context.Context, id primitive.ObjectID, completed bool) error {
+	args := m.Called(ctx, id, completed)
+	return args.Error(0)
+}
+
+func (m *MockTodoRepository) DeleteTodo(ctx context.Context, id primitive.ObjectID) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
+}
